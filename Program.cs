aa1c4bb7573060e14dc0185e74195e6ccfd04bf9
Yusuf Farah <cyufa001@edu.xamk.fi>
2021-01-11 14:40:44 +0200

    Feature A completed and tested.

--- Program.cs
+++ Program.cs
@@ -11,6 +11,13 @@ namespace OOPTask1A
             {
                 Console.Write("Enter your loan: ");
                 string givenValue = Console.ReadLine();
+                //More?
+                Console.Write("More values? Y/N: ");
+                string yourChoice = Console.ReadLine().ToUpper();
+                if (yourChoice.StartsWith("Y"))
+                    stillMore = true;
+                else
+                    stillMore = false;
             } while (stillMore);
         }
     }
