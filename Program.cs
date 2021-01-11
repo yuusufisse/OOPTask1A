3d84673b2c194fc6a92908c6d486dffdea688636
Yusuf Farah <cyufa001@edu.xamk.fi>
2021-01-11 14:33:03 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace OOPTask1A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool stillMore = false;
+            do
+            {
+                Console.Write("Enter your loan: ");
+                string givenValue = Console.ReadLine();
+            } while (stillMore);
+        }
+    }
+}
