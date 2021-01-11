d5769f56460e4e3274b4b7027f95e7a54b551ff4
Yusuf Farah <cyufa001@edu.xamk.fi>
2021-01-11 14:49:30 +0200

    Feature B completed and tested.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace OOPTask1A
 {
@@ -6,11 +7,19 @@ namespace OOPTask1A
     {
         static void Main(string[] args)
         {
+            Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-EN");
             bool stillMore = false;
+            double amountOfLoad;
             do
             {
                 Console.Write("Enter your loan: ");
                 string givenValue = Console.ReadLine();
+                //Type? 
+                while(!Double.TryParse(givenValue, out amountOfLoad) || amountOfLoad < 0)
+                {
+                    Console.Write("Wrong type or negative value, try again: ");
+                    givenValue = Console.ReadLine();
+                }
                 //More?
                 Console.Write("More values? Y/N: ");
                 string yourChoice = Console.ReadLine().ToUpper();
@@ -19,6 +28,8 @@ namespace OOPTask1A
                 else
                     stillMore = false;
             } while (stillMore);
+
+            Console.WriteLine("Given loan was {0:F2}.", amountOfLoad);
         }
     }
 }
