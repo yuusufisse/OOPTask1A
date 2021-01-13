e17cb9583290815b1de5521f2b374038cb843cdf
Yusuf Farah <cyufa001@edu.xamk.fi>
2021-01-13 11:12:09 +0200

    Feature B completed

--- Program.cs
+++ Program.cs
@@ -10,6 +10,13 @@ namespace OOPTask1A
             Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-EN");
             bool stillMore = false;
             double amountOfLoad;
+            double annualIntrestRate;
+            int numberOfYears;
+            int numberOfPayments;
+            double monthlyIntrestRate;
+            double monthlyPayment;
+            double totalPaymentPaid;
+            double totalIntrestPaid;
             do
             {
                 Console.Write("Enter your loan: ");
@@ -20,6 +27,42 @@ namespace OOPTask1A
                     Console.Write("Wrong type or negative value, try again: ");
                     givenValue = Console.ReadLine();
                 }
+
+                Console.Write("Enter your annual intrest rate: ");
+                givenValue = Console.ReadLine();
+                //Type? 
+                while (!Double.TryParse(givenValue, out annualIntrestRate) || annualIntrestRate < 0)
+                {
+                    Console.Write("Wrong type or negative value, try again: ");
+                    givenValue = Console.ReadLine();
+                }
+
+                Console.Write("Number of years: ");
+                givenValue = Console.ReadLine();
+                //Type? 
+                while (!Int32.TryParse(givenValue, out numberOfYears) || numberOfYears < 0)
+                {
+                    Console.Write("Wrong type or negative value, try again: ");
+                    givenValue = Console.ReadLine();
+                }
+
+                monthlyIntrestRate = annualIntrestRate / 12;
+                numberOfPayments = numberOfYears * 12;
+
+                //Now we have all the information needed.
+                monthlyPayment = monthlyIntrestRate * Math.Pow((1 + monthlyIntrestRate), numberOfPayments) * amountOfLoad / (Math.Pow((1 + monthlyIntrestRate), numberOfPayments) - 1);
+                totalPaymentPaid = numberOfPayments * monthlyPayment;
+                totalIntrestPaid = totalPaymentPaid - amountOfLoad;
+
+                Console.WriteLine("Given loan was {0:F2}.", amountOfLoad);
+                Console.WriteLine("Annual intrest rate was {0:F3}.", annualIntrestRate);
+                Console.WriteLine("Number of years was {0}.", numberOfYears);
+                Console.WriteLine("Number of monthss was {0}.", numberOfPayments);
+                Console.WriteLine("Monthly intrest rate was {0:F4}.", monthlyIntrestRate);
+                Console.WriteLine("Monthly payment was {0:F2} euros.", monthlyPayment);
+                Console.WriteLine("You will pay in total {0:F2} euros.", totalPaymentPaid);
+                Console.WriteLine("You will pay intrest in total {0:F2} euros.", totalIntrestPaid);
+
                 //More?
                 Console.Write("More values? Y/N: ");
                 string yourChoice = Console.ReadLine().ToUpper();
@@ -29,7 +72,14 @@ namespace OOPTask1A
                     stillMore = false;
             } while (stillMore);
 
-            Console.WriteLine("Given loan was {0:F2}.", amountOfLoad);
+            //Console.WriteLine("Given loan was {0:F2}.", amountOfLoad);
+            //Console.WriteLine("Annual intrest rate was {0:F3}.", annualIntrestRate);
+            //Console.WriteLine("Number of years was {0}.", numberOfYears);
+            //Console.WriteLine("Number of monthss was {0}.", numberOfPayments);
+            //Console.WriteLine("Monthly intrest rate was {0:F4}.", monthlyIntrestRate);
+            //Console.WriteLine("Monthly payment was {0:F2} euros.", monthlyPayment);
+            //Console.WriteLine("You will pay in total {0:F2} euros.", totalPaymentPaid);
+            //Console.WriteLine("You will pay intrest in total {0:F2} euros.", totalIntrestPaid);
         }
     }
 }
